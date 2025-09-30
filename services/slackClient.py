affb452e1eb3a473deab87788008747c93de9a75
Vinicius Benfica <vinicius.benfica.ruy@gmail.com>
2025-09-30 15:52:19 -0300

    remove Slack notification

--- services/slackClient.py
+++ services/slackClient.py
@@ -8,13 +8,14 @@ logger = logging.getLogger(__name__)
 
 class SlackClient:
     def send_message(self, text: str) -> None:
-        try:
-            resp = requests.post(
-                SLACK_WEBHOOK_URL,
-                json={"text": text},
-                headers={"Content-Type": "application/json"},
-                timeout=10,
-            )
-            resp.raise_for_status()
-        except Exception as e:
-            logger.error(f"Error sending to Slack: {e}")
+        print()
+        # try:
+        #     resp = requests.post(
+        #         SLACK_WEBHOOK_URL,
+        #         json={"text": text},
+        #         headers={"Content-Type": "application/json"},
+        #         timeout=10,
+        #     )
+        #     resp.raise_for_status()
+        # except Exception as e:
+        #     logger.error(f"Error sending to Slack: {e}")
